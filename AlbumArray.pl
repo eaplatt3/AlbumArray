afd67e37ed13fe9dc175712770f8b1492752b34e
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-03-14 12:12:02 -0400

    Update AlbumArray.pl
    DeBugging

--- AlbumArray.pl
+++ AlbumArray.pl
@@ -9,18 +9,19 @@
 
 use strict;
 use warnings;
+#use Data::Dumper qw(Dumper);
 
 #Variables
-my @records = ("A Global Threat", "What the Fuck Will Change?", "1999", "Leftover Crack", "Mediocre Generica", 
-				"2000", "Lunachicks", "Babysitters on Acid", "1990", "Days N' Daze", "Rogue Taxidermy", 
-				"2013", "Dead Milkmen", "Beelzebubba", "1988");
+my $records = qw(A Global Threat, What the Fuck Will Change?, 1999, Leftover Crack, Mediocre Generica, 
+				2000, Lunachicks, Babysitters on Acid, 1990, Days N' Daze, Rogue Taxidermy, 
+				2013, Dead Milkmen, Beelzebubba, 1988);
 my $sortedRecords;
 my @split;
 
 #Array Sorting 
-@split = split (',', @records);
+@split = split (',', $records);
 
 #Loop for Output
 foreach $sortedRecords(@split){
-	print $sortedRecords . "\n";
+	print $sortedRecords,  "\n";
 	}
