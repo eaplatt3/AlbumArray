b099f403a1e4fde85aee8045941a5025ec853ae6
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-03-14 11:26:59 -0400

    Update AlbumArray.pl
    Added spit and print

--- AlbumArray.pl
+++ AlbumArray.pl
@@ -11,10 +11,16 @@ use strict;
 use warnings;
 
 #Variables
-
+my @records qw(A Global Threat,  What the Fuck Will Change?, 1999, Leftover Crack,  Mediocre Generica, 2000,
+				 Lunachicks, Babysitters on Acid, 1990, Days N' Daze, Rogue Taxidermy, 2013, Dead Milkmen, Beelzebubba,
+				1988);
+my $sortedRecords;
+my @split;
 
 #Array Sorting 
-
-
+@split = split (',', @records);
 
 #Loop for Output
+foreach $sortedRecords(@split){
+	print "$sortedRecords";
+	}
